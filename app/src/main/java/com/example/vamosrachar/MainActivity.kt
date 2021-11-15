ca69ae8fa6c014edb3488cf824719b9606d396cf
CaioBrandz <caiobrandz@gmail.com>
2021-11-15 03:59:32 -0300

    init; funcoes dividir,compartilhar e tts

---
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.vamosrachar
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.KeyEvent
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
+
+
+class MainActivity : AppCompatActivity(),TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech? = null
+    private var speaker: FloatingActionButton? = null
+    private var res:TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val n1 = findViewById<EditText>(R.id.editNumber1)
+        val n2 = findViewById<EditText>(R.id.editNumber2)
+        res = findViewById(R.id.res)
+        val share = findViewById<FloatingActionButton>(R.id.ShareButton)
+        speaker = findViewById(R.id.speakerButton)
+
+        n1.setOnKeyListener { v, keyCode, event ->
+            var handled = false
+            if (keyCode == KeyEvent.KEYCODE_DEL || keyCode >= 0) {
+                if (n1.text.toString().trim().isNotEmpty() && n2.text.toString().trim().isNotEmpty()) {
+                    val val1 = n1.text.toString().toFloat()
+                    val val2 = n2.text.toString().toFloat()
+                    val div = val1 / val2
+                    res!!.text = div.toString()
+                }
+            }
+            handled
+        }
+
+        n2.setOnKeyListener { v, keyCode, event ->
+            var handled = false
+            if (keyCode == KeyEvent.KEYCODE_DEL || keyCode >= 0) {
+                if (n1.text.toString().trim().isNotEmpty() && n2.text.toString().trim().isNotEmpty()) {
+                    val val1 = n1.text.toString().toFloat()
+                    val val2 = n2.text.toString().toFloat()
+                    val div = val1 / val2
+                    res!!.text = div.toString()
+                }
+            }
+            handled
+        }
+
+        share.setOnClickListener{
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, res!!.text.toString())
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        speaker!!.isEnabled = false
+        tts = TextToSpeech(this,this)
+        speaker!!.setOnClickListener{
+            speak()
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val result = tts!!.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                Toast.makeText(this, "not suported", Toast.LENGTH_SHORT).show()
+            }else{
+                speaker!!.isEnabled = true
+            }
+        }else{
+            Toast.makeText(this, "falha!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun speak() {
+        val sp = res!!.text.toString()
+        tts!!.speak(sp, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onDestroy() {
+        if (tts != null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+}
